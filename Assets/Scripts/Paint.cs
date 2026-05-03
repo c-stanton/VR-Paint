eee9e165a2988c000a25340fa66b3319f93594d6
Chloe Stanton <cstanton5@ewu.edu>
2026-05-03 12:29:38 -0700

    Fixed trail renderer

--- Assets/Scripts/Paint.cs
+++ Assets/Scripts/Paint.cs
@@ -1,106 +1,57 @@
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Paint : MonoBehaviour
 {
     public Transform tip;
     public TrailRenderer brushStroke;
-    private TrailRenderer currentBrushStroke;
-    private Stack<TrailRenderer> previousStroke = new Stack<TrailRenderer>();
     private AnimateHandController aController = null;
-    private Color32 currentColor = Color.red;
-    int drawCount = 0;
-    bool drawColor = true;
-    
+    private TrailRenderer currentStroke;
+
     void Start()
     {
-        tip.parent.GetComponent<Renderer>().material.color = currentColor;
+        if (tip != null)
+            tip.parent.GetComponent<Renderer>().material.color = Color.red;
     }
 
     void Update()
     {
-        if (aController != null && drawColor)
-        {
-            if (aController.GetGripValue() == 1 && aController.GetTriggerValue() == 1)
-            {
-                currentBrushStroke = Instantiate(brushStroke, tip.position, tip.rotation, tip);
-                currentBrushStroke.material.color = currentColor;
-                previousStroke.Push(currentBrushStroke);
-            }
-            
-            drawCount = 1;
-            drawColor = false;
-        }
+        if (aController == null) return;
 
-        if (!drawColor)
-        {
-            if (aController != null && drawCount ==  1)
-            {
-                if (aController.GetGripValue() == 1 && aController.GetTriggerValue() != 1)
-                {
-                    if (currentBrushStroke != null)
-                    {
-                        if (currentBrushStroke.transform.parent != null)
-                        {
-                            currentBrushStroke.transform.parent = null;
-                            drawCount = 0;
-                        }
-                    }
-                }
-            }
-        }
+        float triggerValue = aController.GetTriggerValue();
 
-        if (!drawColor)
+        if (triggerValue > 0.1f && currentStroke == null)
         {
-            if (aController != null && drawCount == 0)
-            {
-                if (aController.GetGripValue() == 1 && aController.GetTriggerValue() == 1)
-                {
-                    currentBrushStroke = Instantiate(brushStroke, tip.position, tip.rotation, tip);
-                    currentBrushStroke.material.color = currentColor;
-                    previousStroke.Push(currentBrushStroke);
-                    drawCount = 1;
-                }
-            }
+            currentStroke = Instantiate(brushStroke, tip.position, tip.rotation, tip);
+        }
+        
+        if (triggerValue <= 0.1f && currentStroke != null)
+        {
+            currentStroke.transform.parent = null;
+            currentStroke = null;
         }
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (!other.gameObject.CompareTag("VRHand"))
-        {
-            return;
-        }
+        if (!other.gameObject.CompareTag("VRHand")) return;
 
         AnimateHandController aHandController = other.gameObject.GetComponent<AnimateHandController>();
         
-        if (aController != null)
+        if (aHandController != null)
         {
             aController = aHandController;
-            drawColor = true;
         }
     }
 
     private void OnTriggerExit(Collider other)
     {
-        if (!other.gameObject.CompareTag("VRHand")) 
-        { 
-            return; 
-        }
+        if (!other.gameObject.CompareTag("VRHand")) return;
 
-        if (currentBrushStroke != null)
+        if (currentStroke != null)
         {
-            if (currentBrushStroke.transform.parent != null)
-            {
-                currentBrushStroke.transform.parent = null;
-            }
-
-            if (aController != null)
-            {
-                aController = null;
-            }
-
-            drawCount = 0;
+            currentStroke.transform.parent = null;
+            currentStroke = null;
         }
+        aController = null;
     }
 }
